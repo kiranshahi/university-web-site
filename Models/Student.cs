40fda9015ee4cb4850f3486fbd5d7a150888eb37
Kiran Shahi <kiran.shahi.np@gmail.com>
2017-12-28 17:22:28 +0545

    Seed data has been added to DbInitializer class.

--- Models/Student.cs
+++ Models/Student.cs
@@ -7,7 +7,7 @@ namespace ContosoUniversity.Models
     {
         public int ID { get; set; }
         public string LastName { get; set; }
-        public string FirstName { get; set; }
+        public string FirstMidName { get; set; }
         public DateTime EnrollmentDate { get; set; }
 
         public ICollection<Enrollment> Enrollments {get; set;}
