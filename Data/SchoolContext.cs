e779acc39353422fc612ee47c423efecacd2f809
Kiran Shahi <kiran.shahi.np@gmail.com>
2017-12-28 16:40:18 +0545

    dbcontext class has been added.

---
+++ Data/SchoolContext.cs
@@ -0,0 +1,16 @@
+using ContosoUniversity.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContosoUniversity.Data
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext(DbContextOptions<SchoolContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Student> Students { get; set; }
+    }
+}
