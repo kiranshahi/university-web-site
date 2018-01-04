2ccb5617eaf75fb36f1562c5ceaf5b4a6f431eb6
Kiran Shahi <kiran.shahi.np@gmail.com>
2018-01-04 16:16:56 +0545

    String length attribute has been applied to FirstMidName and LastName property.

--- Models/Student.cs
+++ Models/Student.cs
@@ -7,7 +7,9 @@ namespace ContosoUniversity.Models
     public class Student
     {
         public int ID { get; set; }
+        [StringLength(50)]
         public string LastName { get; set; }
+        [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]        
         public string FirstMidName { get; set; }
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
