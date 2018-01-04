f14315fa497a9d0e59b20d1aa6dd42318ec2b104
Kiran Shahi <kiran.shahi.np@gmail.com>
2018-01-04 16:27:31 +0545

    new migration generated.

--- Migrations/20180104104139_ColumnFirstName.Designer.cs
+++ Migrations/20180104104139_ColumnFirstName.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace ContosoUniversity.Migrations
 {
     [DbContext(typeof(SchoolContext))]
-    partial class SchoolContextModelSnapshot : ModelSnapshot
+    [Migration("20180104104139_ColumnFirstName")]
+    partial class ColumnFirstName
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
