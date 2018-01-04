77ac5c3c6f9d47c66731b6c1902f5ef4a093ea54
Kiran Shahi <kiran.shahi.np@gmail.com>
2018-01-04 17:15:44 +0545

    Course and Instructor entity updated.

--- Models/Instructor.cs
+++ Models/Instructor.cs
@@ -33,7 +33,7 @@ namespace ContosoUniversity.Models
                 return LastName + ", " + FirstMidName;
             }
         }
-        public ICollection<CourseAssignment> CourseAssignment { get; set; }
+        public ICollection<CourseAssignment> CourseAssignments { get; set; }
         public OfficeAssignment OfficeAssignment { get; set; }
     }
 }
