4c8aa420f0635df63a61a04048fdbd8362fdfe13
Kiran Shahi <kiran.shahi.np@gmail.com>
2018-01-04 16:25:19 +0545

    column attribute has been added to FirstMidName property.

--- Models/Student.cs
+++ Models/Student.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ContosoUniversity.Models
 {
@@ -9,7 +10,8 @@ namespace ContosoUniversity.Models
         public int ID { get; set; }
         [StringLength(50)]
         public string LastName { get; set; }
-        [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]        
+        [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
+        [Column("FirstName")]
         public string FirstMidName { get; set; }
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
