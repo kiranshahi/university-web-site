a6edc39204e6fb55951836f44fd21371fc0e70da
Kiran Shahi <kiran.shahi.np@gmail.com>
2018-01-04 16:09:00 +0545

    data type and display format of EnrollmentDate property has been modified.

--- Models/Student.cs
+++ Models/Student.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace ContosoUniversity.Models
 {
@@ -8,8 +9,10 @@ namespace ContosoUniversity.Models
         public int ID { get; set; }
         public string LastName { get; set; }
         public string FirstMidName { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime EnrollmentDate { get; set; }
 
-        public ICollection<Enrollment> Enrollments {get; set;}
+        public ICollection<Enrollment> Enrollments { get; set; }
     }
 }
