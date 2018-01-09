384d944ba6763d5cb15ebf5ed202d8607de2bbd7
Kiran Shahi <kiran.shahi.np@gmail.com>
2018-01-09 14:09:12 +0545

    About method of home controller has been updated.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,6 +8,7 @@ using ContosoUniversity.Models;
 using Microsoft.EntityFrameworkCore;
 using ContosoUniversity.Data;
 using ContosoUniversity.Models.SchoolViewModels;
+using System.Data.Common;
 
 namespace ContosoUniversity.Controllers
 {
@@ -25,15 +26,36 @@ namespace ContosoUniversity.Controllers
 
         public async Task<ActionResult> About()
         {
-            IQueryable<EnrollmentDateGroup> data =
-            from student in _context.Students
-            group student by student.EnrollmentDate into dateGroup
-            select new EnrollmentDateGroup()
+            List<EnrollmentDateGroup> groups = new List<EnrollmentDateGroup>();
+            var conn = _context.Database.GetDbConnection();
+            try
             {
-                EnrollmentDate = dateGroup.Key,
-                StudentCount = dateGroup.Count()
-            };
-            return View(await data.AsNoTracking().ToListAsync());
+                await conn.OpenAsync();
+                using (var command = conn.CreateCommand())
+                {
+                    string query = "SELECT EnrollmentDate, COUNT(*) AS StudentCount "
+                    + "FROM Person "
+                    + "WHERE Discriminator = 'Student' "
+                    + "GROUP BY EnrollmentDate";
+                    command.CommandText = query;
+                    DbDataReader reader = await command.ExecuteReaderAsync();
+
+                    if (reader.HasRows)
+                    {
+                        while (await reader.ReadAsync())
+                        {
+                            var row = new EnrollmentDateGroup { EnrollmentDate = reader.GetDateTime(0), StudentCount = reader.GetInt32(1) };
+                            groups.Add(row);
+                        }
+                    }
+                    reader.Dispose();
+                }
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return View(groups);
         }
 
         public IActionResult Contact()
