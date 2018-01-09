4cadca4fa813e9842da4ceb0035c8bb052dbefd0
Kiran Shahi <kiran.shahi.np@gmail.com>
2018-01-09 12:53:14 +0545

    migration added.

--- Migrations/20180109064453_Inheritance.Designer.cs
+++ Migrations/20180109064453_Inheritance.Designer.cs
@@ -13,9 +13,10 @@ using System;
 namespace ContosoUniversity.Migrations
 {
     [DbContext(typeof(SchoolContext))]
-    partial class SchoolContextModelSnapshot : ModelSnapshot
+    [Migration("20180109064453_Inheritance")]
+    partial class Inheritance
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
