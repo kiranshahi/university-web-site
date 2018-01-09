88b5224013a253b3b720bddaca359d19cc7d571f
Kiran Shahi <kiran.shahi.np@gmail.com>
2018-01-09 14:18:26 +0545

    UpdateCourseCredits method has been added.

--- Controllers/CoursesController.cs
+++ Controllers/CoursesController.cs
@@ -168,5 +168,24 @@ namespace ContosoUniversity.Controllers
                                    select d;
             ViewBag.DepartmentID = new SelectList(departmentsQuery.AsNoTracking(), "DepartmentID", "Name", selectedDepartment);
         }
+
+        public IActionResult UpdateCourseCredits()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateCourseCredits(int? multiplier)
+        {
+            if (multiplier != null)
+            {
+                ViewData["RowsAffected"] =
+                await _context.Database.ExecuteSqlCommandAsync(
+                    "UPDATE Course SET Credits = Credits * {0}",
+                    parameters: multiplier
+                );
+            }
+            return View();
+        }
     }
 }
