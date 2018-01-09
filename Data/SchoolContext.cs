7676142cf772af59ea2fd98a43e16d149a1dce14
Kiran Shahi <kiran.shahi.np@gmail.com>
2018-01-09 12:29:14 +0545

    dbcontext updated.

--- Data/SchoolContext.cs
+++ Data/SchoolContext.cs
@@ -16,6 +16,7 @@ namespace ContosoUniversity.Data
         public DbSet<Instructor> Instructors { get; set; }
         public DbSet<OfficeAssignment> OfficeAssignments { get; set; }
         public DbSet<CourseAssignment> CourseAssignment { get; set; }
+        public DbSet<Person> People { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Course>().ToTable("Course");
@@ -25,6 +26,7 @@ namespace ContosoUniversity.Data
             modelBuilder.Entity<Instructor>().ToTable("Instructor");
             modelBuilder.Entity<OfficeAssignment>().ToTable("OfficeAssignment");
             modelBuilder.Entity<CourseAssignment>().ToTable("CourseAssignment");
+            modelBuilder.Entity<Person>().ToTable("Person");
 
             modelBuilder.Entity<CourseAssignment>()
             .HasKey(c => new { c.CourseID, c.InstructorID });
